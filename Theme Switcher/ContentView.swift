db09935b21cb76228e4d41cb2d7e84a5ab2e7184
hellodamii <daremiarchs@gmail.com>
2024-10-26 11:58:04 +0100

    Initial Commit

---
+++ Theme Switcher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Theme Switcher
+//
+//  Created by Busha on 26/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Theme Switcher/Theme_SwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Theme_SwitcherApp.swift
+//  Theme Switcher
+//
+//  Created by Busha on 26/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Theme_SwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
